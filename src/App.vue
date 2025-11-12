358ceec15c04601c9d5df549a3f542c1dcfff081
Abdul Raziq Quraishi <99569578+raziq-quraishi@users.noreply.github.com>
2025-11-12 08:16:10 +0430

    Adding Filtering Button Add, Todo and Done buttons

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import TaskForm from './components/TaskForm.vue';
 import TaskList from './components/TaskList.vue';
-import type { Task } from './types';
+import FilterButton from './components/FilterButton.vue';
+import type { Task, TaskFilter } from './types';
 import { computed, ref } from 'vue';
 const message = ref('Vue Tasks App')
 const tasks = ref<Task[]>([])
+const filter =ref<TaskFilter>("all")
+
+
 const totalDone = computed(()=> tasks.value.reduce((total, task)=>task.done? total + 1: total,0))
 function addTask(newTask:string){
     tasks.value.unshift({
@@ -13,6 +17,19 @@ function addTask(newTask:string){
         done:false
     })
 }
+
+const filteredTask=computed(()=>{
+    switch(filter.value){
+        case 'all':
+        return tasks.value;
+        case 'todo':
+            return tasks.value.filter(task=>!task.done);
+        case 'done':
+            return tasks.value.filter(task=>task.done)
+    }
+    return tasks.value;
+})
+
 function toggleDone(taskId:string){
     const task = tasks.value.find(task=>task.id ===taskId)
     if(task){
@@ -25,6 +42,9 @@ function removeTask(id:string){
         tasks.value.splice(index,1)
     }
 }
+function setFilter(value:TaskFilter){
+filter.value= value;
+}
 
 </script>
 
@@ -34,7 +54,13 @@ function removeTask(id:string){
         <task-form @add-task="addTask"/>
         <h3 v-if="!tasks.length">Add a task to get started.</h3>
         <h3 v-else>{{ totalDone }} / {{ tasks.length }} tasks completed.</h3>
-        <task-list :tasks="tasks" @toggle-done="toggleDone" @delete-task="removeTask"/>
+        <div class="button-container" v-if="tasks.length">
+            <FilterButton :currentFilter="filter" filter="all" @set-filter="setFilter"/>
+            <FilterButton :currentFilter="filter" filter="todo" @set-filter="setFilter"/>
+            <FilterButton :currentFilter="filter" filter="done" @set-filter="setFilter"/>
+           
+        </div>
+        <task-list :tasks="filteredTask" @toggle-done="toggleDone" @delete-task="removeTask"/>
           
     </main>
 </template>
@@ -48,6 +74,7 @@ main{
 .button-container{
     display: flex;
     justify-content: end;
+    gap: .5rem;
 }
 
 </style>
---
+++ src/components/FilterButton.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import type { TaskFilter } from '../types';
+
+const props = defineProps<{
+    filter:TaskFilter,
+    currentFilter:TaskFilter
+}>()
+const emit = defineEmits<{
+    setFilter:[filter: TaskFilter]
+}>()
+</script>
+
+<template>
+    <button class="secondary" :class="{contrast: props.currentFilter===props.filter}" @click="emit('setFilter', props.filter)">{{ props.filter }}</button>
+</template>
+
+<style  scoped>
+button{
+    text-transform: capitalize;
+}
+</style>
